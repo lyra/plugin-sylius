59413c3aa3d88f699de3ad0682ca28f4527ddc53
Alima Grine <alima.grine@lyra-network.com>
2025-09-29 16:00:28 +0200

    [BOT] Migrate PayZen 2.1.1 to GitHub.

--- LyranetworkPayzen/src/Sdk/Tools.php
+++ LyranetworkPayzen/src/Sdk/Tools.php
@@ -30,7 +30,7 @@ class Tools
 
     private static $CMS_IDENTIFIER = 'Sylius_2.x';
     private static $SUPPORT_EMAIL = 'https://payzen.io/fr-FR/support/';
-    private static $PLUGIN_VERSION = '2.1.0';
+    private static $PLUGIN_VERSION = '2.1.1';
     private static $GATEWAY_VERSION = 'V2';
     private static $REST_URL = 'https://api.payzen.eu/api-payment/';
     private static $STATIC_URL = 'https://static.payzen.eu/static/';
